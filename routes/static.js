b94a7920ec16542be34659a8b4d869d1fb5fe8b1
Stacy Yarrington <syarrington@enuit.com>
2025-05-08 07:54:33 -0600

    Week 1 initial

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); 
+const router = express.Router();//router is a function
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
+router.use(express.static("public")); //use the express.static function, static resources will be in this folder
+router.use("/css", express.static(__dirname + "public/css"));//any route that contains css is in public/css
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
-module.exports = router;
+module.exports = router; //exports the router object with all of these statements for use in other areas of the application **!!!**
 
 
 
--- server.js
+++ server.js
@@ -6,14 +6,33 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config()
-const app = express()
+const app = express() //express as a function is defined to app variable
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")//ejs will be the view engine, all views stored in a views folder
+app.use(expressLayouts)//use the exxpress-ejs-layouts
+app.set("layout", "./layouts/layout") // not at views root, when the express ejs layout looks for the basic template, it will find a layouts folder , template named layout
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static) //takes the routes/static config and applies to the application
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+//The express application will watch the get object for a particular route. / is the route being satched
+//function(req, res) = js function that takes request and response as parameters
+//res.render() response object, render is an express function that will retrieve the index view
+//title home is an object to send- used in head partial file
+
+
 
 /* ***********************
  * Local Server Information
